78fece21d0c27e7a22215cf7da60c393efd1f2bf
Иван <99138124+BA999ZAI@users.noreply.github.com>
2022-04-25 20:06:06 +0300

    Просто ничего не делал

--- Second version/index.js
+++
@@ -1,7 +0,0 @@
-function showAlert(){
-    const value = document.getElementById('pi').innerHTML;
-    alert(value)
-}
-function onChangeText(value){
-    document.getElementById('pi').innerHTML = value;
-}
---
+++ Second-version/index.js
@@ -0,0 +1,7 @@
+function showAlert(){
+    const value = document.getElementById('pi').innerHTML;
+    alert(value)
+}
+function onChangeText(value){
+    document.getElementById('pi').innerHTML = value;
+}
